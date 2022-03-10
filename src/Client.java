59594f4b961f1866b0659074dcfd93825f37a84d
JPampers <jprodriguezcano99@gmail.com>
2022-03-10 17:55:45 -0600

    funcionalidad basica de cliente y conexion con servidor

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,37 @@
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class Client {
+    //metodo principal
+    public static void main(String[] args) throws IOException{
+        String host = "127.0.0.1";
+        int puerto = 5030;
+        DataInputStream in;
+        DataOutputStream out;
+
+
+        try {
+            Socket socketCliente = new Socket(host, puerto);
+            in = new DataInputStream(socketCliente.getInputStream());
+            out = new DataOutputStream(socketCliente.getOutputStream());
+
+            out.writeUTF("Cliente concectado con servidor");
+
+            String mensaje = in.readUTF();
+
+            System.out.println(mensaje);
+
+            socketCliente.close();
+
+
+        }catch (IOException ex){
+
+        }
+
+
+
+    }
 }
